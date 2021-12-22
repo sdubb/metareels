50c919f53eec7076332d0d154ec25c5ff977124d
SHUBHAM DUBEY <shuubhamdubey3131@gmail.com>
2021-12-22 18:49:14 +0000

    Updates

---
+++ precache-manifest.daf9d14457a886b35decfaccdc2bf882.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3845706dad77ab2ffe3d828caefba953",
+    "url": "/metareels/index.html"
+  },
+  {
+    "revision": "447834cc1536dc26fb5a",
+    "url": "/metareels/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "6ce56070fde937bd5bf9",
+    "url": "/metareels/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "447834cc1536dc26fb5a",
+    "url": "/metareels/static/js/2.e453dd40.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "/metareels/static/js/2.e453dd40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce56070fde937bd5bf9",
+    "url": "/metareels/static/js/main.d56377dd.chunk.js"
+  },
+  {
+    "revision": "004f041c1d20a7f7d33d",
+    "url": "/metareels/static/js/runtime-main.fb1e5d2d.js"
+  },
+  {
+    "revision": "9e02a88e54fb14899a2ea3c60bf8fa35",
+    "url": "/metareels/static/media/dvideo.9e02a88e.png"
+  }
+]);
